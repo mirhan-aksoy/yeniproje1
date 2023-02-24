7fdd8bada1c9b10dc26d971586023e7d37e54008
mirhan-aksoy <mirhanaksoy63@gmail.com>
2023-02-24 22:09:50 +0300

    v3

--- src/main/java/GitGitup.java
+++ src/main/java/GitGitup.java
@@ -2,5 +2,6 @@ public class GitGitup {
     public static void main(String[] args) {
         System.out.println("v1 yeni proje");
         System.out.println("v2 komutları");
+        System.out.println("v3 komutları");
     }
 }
