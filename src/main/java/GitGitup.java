0c22184029d75c3576951cffb7fea9d6736bf391
mirhan-aksoy <mirhanaksoy63@gmail.com>
2023-02-24 22:15:13 +0300

    deneme v4

--- src/main/java/GitGitup.java
+++ src/main/java/GitGitup.java
@@ -3,5 +3,6 @@ public class GitGitup {
         System.out.println("v1 yeni proje");
         System.out.println("v2 komutları");
         System.out.println("v3 komutları");
+        System.out.println("v4 deneme komutları");
     }
 }
